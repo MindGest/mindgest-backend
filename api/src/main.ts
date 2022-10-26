8aff763ce6622e5cc8e90596f25eb2620702165f
Pedro Rodrigues <pedromiguelrodrigues2000@gmail.com>
2022-10-26 16:20:07 +0100

    Adding docker container support

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,16 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 import api from './core/api'
 import log from './utils/logger'
 import docs from './utils/docs'
 
 // Fetch Environment Variables
-const PORT = 8080
-const HOST = 'localhost'
+const HOST = String(process.env.HOST)
+const PORT = Number(process.env.PORT)
 
 // Run Server
 api.listen(PORT, HOST, () => {
     log.info(`MindGest API is live at http://${HOST}:${PORT}/api`)
-
     docs(api, HOST, PORT)
 })
