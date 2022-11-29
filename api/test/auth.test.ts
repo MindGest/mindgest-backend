6fb0e1b82e7dfc965a592be5f1d8848a2ecb785d
DreSilva <DreSilva@users.noreply.github.com>
2022-11-29 17:27:36 +0000

    Apply formatting changes

--- api/test/auth.test.ts
+++ api/test/auth.test.ts
@@ -11,28 +11,26 @@ import app from "../src/main"
 describe("Test if auth is working", () => {
   it("Should not login", async () => {
     const payload = {
-      "email": "johndoe@student.dei.uc.pt",
-      "password": "password1234"
+      email: "johndoe@student.dei.uc.pt",
+      password: "password1234",
     }
     const result = await request(app)
       .post("/api/auth/login")
       .send(payload)
-      .set('Content-Type','application/json')
-      .set('Accept', 'application/json')
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
     expect(result.status).toEqual(StatusCodes.NOT_FOUND)
   })
   it("Should login", async () => {
     const payload = {
-      "email": "andresilva@student.dei.uc",
-      "password": "password1234"
+      email: "andresilva@student.dei.uc",
+      password: "password1234",
     }
     const result = await request(app)
       .post("/api/auth/login")
       .send(payload)
-      .set('Content-Type','application/json')
-      .set('Accept', 'application/json')
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
     expect(result.status).toEqual(StatusCodes.OK)
-  })//user has to be verified and it has to be approved by a therapist or admin b4 this works
+  }) //user has to be verified and it has to be approved by a therapist or admin b4 this works
 })
-
-
