b733ab59a00f7f1e04c38e2209562f95708aea6c
DreSilva <2000.andre.silva@gmail.com>
2022-11-29 17:23:16 +0000

    Simple tests

---
+++ api/test/auth.test.ts
@@ -0,0 +1,38 @@
+import request from "supertest"
+import dotenv from "dotenv"
+import { StatusCodes } from "http-status-codes"
+
+import { describe, expect, it } from "@jest/globals"
+
+dotenv.config()
+
+import app from "../src/main"
+
+describe("Test if auth is working", () => {
+  it("Should not login", async () => {
+    const payload = {
+      "email": "johndoe@student.dei.uc.pt",
+      "password": "password1234"
+    }
+    const result = await request(app)
+      .post("/api/auth/login")
+      .send(payload)
+      .set('Content-Type','application/json')
+      .set('Accept', 'application/json')
+    expect(result.status).toEqual(StatusCodes.NOT_FOUND)
+  })
+  it("Should login", async () => {
+    const payload = {
+      "email": "andresilva@student.dei.uc",
+      "password": "password1234"
+    }
+    const result = await request(app)
+      .post("/api/auth/login")
+      .send(payload)
+      .set('Content-Type','application/json')
+      .set('Accept', 'application/json')
+    expect(result.status).toEqual(StatusCodes.OK)
+  })//user has to be verified and it has to be approved by a therapist or admin b4 this works
+})
+
+
