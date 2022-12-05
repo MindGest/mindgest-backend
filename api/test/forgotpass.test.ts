159470a3b5f6c3e3ddc820bc79a65dc6ad665389
DreSilva <DreSilva@users.noreply.github.com>
2022-12-05 14:44:25 +0000

    Apply formatting changes

--- api/test/forgotpass.test.ts
+++ api/test/forgotpass.test.ts
@@ -11,11 +11,11 @@ import app from "../src/main"
 describe("x.0 forgot password and user exists", () => {
   it("x.0.0 existing user sends request to change password", async () => {
     const payload = {
-      "email": "johndoe@student.dei.uc.pt",
-      "callback": "http://frontend.com/password-reset-page"
-    }    
+      email: "johndoe@student.dei.uc.pt",
+      callback: "http://frontend.com/password-reset-page",
+    }
     const message = {
-      "message": "The user does not exist!"
+      message: "The user does not exist!",
     } //the message is incorrect
     const result = await request(app)
       .post("/api/auth/forgot-password")
@@ -30,11 +30,11 @@ describe("x.0 forgot password and user exists", () => {
 describe("x.2 forgot password and user doesn't exist", () => {
   it("x.2.0 user is not in the database so it cannot recover password", async () => {
     const payload = {
-      "email": "john@student.dei.uc.pt",
-      "callback": "http://frontend.com/password-reset-page"
-    }    
+      email: "john@student.dei.uc.pt",
+      callback: "http://frontend.com/password-reset-page",
+    }
     const message = {
-      "message": "The user does not exist!"
+      message: "The user does not exist!",
     }
     const result = await request(app)
       .post("/api/auth/forgot-password")
@@ -43,6 +43,5 @@ describe("x.2 forgot password and user doesn't exist", () => {
       .set("Accept", "application/json")
     expect(result.status).toEqual(StatusCodes.BAD_REQUEST)
     expect(result.body).toEqual(message)
-    
   })
 })
--- api/test/login.test.ts
+++ api/test/login.test.ts
@@ -11,11 +11,11 @@ import app from "../src/main"
 describe("1.2 the user does not exist", () => {
   it("1.2.0 login with a user that does not exist", async () => {
     const payload = {
-      "email": "email@student.dei.uc.pt",
-      "password": "password1234",
+      email: "email@student.dei.uc.pt",
+      password: "password1234",
     }
     const message = {
-      "message": "The user does not exist!"
+      message: "The user does not exist!",
     }
     const result = await request(app)
       .post("/api/auth/login")
@@ -30,11 +30,11 @@ describe("1.2 the user does not exist", () => {
 describe("1.3 wrong password", () => {
   it("1.3.0 wrong password with an existing user", async () => {
     const payload = {
-      "email": "johndoe@student.dei.uc.pt",
-      "password": "password1235",
+      email: "johndoe@student.dei.uc.pt",
+      password: "password1235",
     }
     const message = {
-      "message": "Login. Invalid credentials"
+      message: "Login. Invalid credentials",
     }
     const result = await request(app)
       .post("/api/auth/login")
@@ -48,8 +48,8 @@ describe("1.3 wrong password", () => {
 describe("1.4 Test if login is working", () => {
   it("1.4.0 Should login", async () => {
     const payload = {
-      "email": "johndoe@student.dei.uc.pt",
-      "password": "password1234",
+      email: "johndoe@student.dei.uc.pt",
+      password: "password1234",
     }
     const result = await request(app)
       .post("/api/auth/login")
