9dd456022cc3ef218c5a0e20ed01fac3ebe6e0f9
DreSilva <2000.andre.silva@gmail.com>
2022-12-05 14:43:31 +0000

    new login and forgotpass tests

--- api/test/login.test.ts
+++ api/test/login.test.ts
@@ -8,11 +8,14 @@ dotenv.config()
 
 import app from "../src/main"
 
-describe("Test if auth login is working", () => {
-  it("Should not login", async () => {
+describe("1.2 the user does not exist", () => {
+  it("1.2.0 login with a user that does not exist", async () => {
     const payload = {
-      email: "johndoe@student.dei.uc.pt",
-      password: "password1234",
+      "email": "email@student.dei.uc.pt",
+      "password": "password1234",
+    }
+    const message = {
+      "message": "The user does not exist!"
     }
     const result = await request(app)
       .post("/api/auth/login")
@@ -20,11 +23,33 @@ describe("Test if auth login is working", () => {
       .set("Content-Type", "application/json")
       .set("Accept", "application/json")
     expect(result.status).toEqual(StatusCodes.NOT_FOUND)
+    expect(result.body).toEqual(message)
   })
-  it("Should login", async () => {
+})
+
+describe("1.3 wrong password", () => {
+  it("1.3.0 wrong password with an existing user", async () => {
     const payload = {
-      email: "andresilva@student.dei.uc",
-      password: "password1234",
+      "email": "johndoe@student.dei.uc.pt",
+      "password": "password1235",
+    }
+    const message = {
+      "message": "Login. Invalid credentials"
+    }
+    const result = await request(app)
+      .post("/api/auth/login")
+      .send(payload)
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+    expect(result.status).toEqual(StatusCodes.UNAUTHORIZED)
+  })
+})
+
+describe("1.4 Test if login is working", () => {
+  it("1.4.0 Should login", async () => {
+    const payload = {
+      "email": "johndoe@student.dei.uc.pt",
+      "password": "password1234",
     }
     const result = await request(app)
       .post("/api/auth/login")
@@ -32,5 +57,8 @@ describe("Test if auth login is working", () => {
       .set("Content-Type", "application/json")
       .set("Accept", "application/json")
     expect(result.status).toEqual(StatusCodes.OK)
+    expect(result.body).toHaveProperty("message", "component name")
+    expect(result.body).toHaveProperty("accessToken")
+    expect(result.body).toHaveProperty("refreshToken")
   }) //user has to be verified and it has to be approved by a therapist or admin b4 this works
 })
