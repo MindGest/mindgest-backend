d2b75d0aeff34353c2561289fffabf5083b05cda
Eurico-98 <Eurico-98@users.noreply.github.com>
2022-12-20 14:55:56 +0000

    Apply formatting changes

--- api/test/process/edit.test.ts
+++ api/test/process/edit.test.ts
@@ -9,20 +9,16 @@ dotenv.config()
 import app from "../../src/main"
 
 describe("(y+4).0 test process edit", () => {
-
   it("(y+4).0.0 test process edit successfully", async () => {
     const payload = {
-        token: "<therapist_auth_token>",
-        therapistId: 0,
-        speciality: "",
-        remarks: "this is just a test",
-        colaborators: [
-          0
-        ]
-      
+      token: "<therapist_auth_token>",
+      therapistId: 0,
+      speciality: "",
+      remarks: "this is just a test",
+      colaborators: [0],
     }
     const message = {
-        message: "Process Edited"
+      message: "Process Edited",
     }
     const result = await request(app)
       .post("/api/process/edit")
@@ -35,17 +31,14 @@ describe("(y+4).0 test process edit", () => {
 
   it("(y+4).1.0 test process edit without permission", async () => {
     const payload = {
-        token: "<therapist_auth_token>",
-        therapistId: 0,
-        speciality: "",
-        remarks: "this is just a test",
-        colaborators: [
-          0
-        ]
-      
+      token: "<therapist_auth_token>",
+      therapistId: 0,
+      speciality: "",
+      remarks: "this is just a test",
+      colaborators: [0],
     }
     const message = {
-        message: "User doesn't have authorization"
+      message: "User doesn't have authorization",
     }
     const result = await request(app)
       .post("/api/process/edit")
@@ -58,17 +51,14 @@ describe("(y+4).0 test process edit", () => {
 
   it("(y+4).2.0 test process edit with expired token", async () => {
     const payload = {
-        token: "<expired_token>",
-        therapistId: 0,
-        speciality: "",
-        remarks: "Verification token invalid or expired",
-        colaborators: [
-          0
-        ]
-      
+      token: "<expired_token>",
+      therapistId: 0,
+      speciality: "",
+      remarks: "Verification token invalid or expired",
+      colaborators: [0],
     }
     const message = {
-        message: "User doesn't have authorization"
+      message: "User doesn't have authorization",
     }
     const result = await request(app)
       .post("/api/process/edit")
