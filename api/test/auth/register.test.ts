bb110abcdd7997c5e94f69013a07d7e3a25b02cf
DreSilva <2000.andre.silva@gmail.com>
2023-01-19 17:51:20 +0000

    auth tests update

--- api/test/auth/register.test.ts
+++ api/test/auth/register.test.ts
@@ -8,7 +8,7 @@ dotenv.config()
 
 import app from "../../src/main"
 
-describe("0.0 Test admin registration", () => {
+describe("0.0 Test registration", () => {
   it("0.0.0 test - Validate admin register", async () => {
     const payload = {
       role: "admin",
@@ -33,16 +33,37 @@ describe("0.0 Test admin registration", () => {
     expect(result.status).toEqual(StatusCodes.OK)
     expect(result.body).toEqual(message)
   })
-})
+  it("0.0.1 test - Repeat registration", async () => {
+    const payload = {
+      role: "admin",
+      name: "Sara Brito",
+      email: "sarab@student.dei.uc",
+      password: "password1234",
+      address: "Ferreira Borges, n 10",
+      birthDate: "1988-11-21T0:00:00.000Z",
+      phoneNumber: 919191000,
+    }
 
-describe("0.1 Test therapist registration", () => {
-  it("0.1.0 test - Validate therapist register", async () => {
+    // in json format
+    const message = {
+      message: "A user with this email already exists!"
+    }
+
+    const result = await request(app)
+      .post("/api/auth/register")
+      .send(payload)
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+    expect(result.status).toEqual(StatusCodes.CONFLICT)
+    expect(result.body).toEqual(message)
+  })
+  it("0.0.2 test - Validate therapist register", async () => {
     const payload = {
       role: "therapist",
       name: "John Doe",
       email: "johndoe@student.dei.uc",
       password: "password1234",
-      address: "Wall Streed",
+      address: "Wall Street",
       birthDate: "1990-11-21T23:50:28.538Z",
       phoneNumber: 9219231942,
       healthSystem: "adse",
@@ -63,10 +84,7 @@ describe("0.1 Test therapist registration", () => {
     expect(result.status).toEqual(StatusCodes.OK)
     expect(result.body).toEqual(message)
   })
-})
-
-describe("0.2 Test security personnel registration", () => {
-  it("0.2.0 test - Validate security register", async () => {
+  it("0.0.3 test - Validate security register", async () => {
     const payload = {
       role: "guard",
       name: "Pedro Oblíquo",
@@ -91,10 +109,7 @@ describe("0.2 Test security personnel registration", () => {
     expect(result.status).toEqual(StatusCodes.OK)
     expect(result.body).toEqual(message)
   })
-})
-
-describe("0.3 Test accountant registration", () => {
-  it("0.3.0 test - Validate accountant register", async () => {
+  it("0.0.4 test - Validate accountant register", async () => {
     const payload = {
       role: "accountant",
       name: "Carolina Damásio",
@@ -119,10 +134,7 @@ describe("0.3 Test accountant registration", () => {
     expect(result.status).toEqual(StatusCodes.OK)
     expect(result.body).toEqual(message)
   })
-})
-
-describe("0.4 Test intern registration", () => {
-  it("0.4.0 test - Validate intern register", async () => {
+  it("0.0.5 test - Validate intern register", async () => {
     const payload = {
       role: "intern",
       name: "Maria Menezes",
@@ -145,78 +157,4 @@ describe("0.4 Test intern registration", () => {
     expect(result.status).toEqual(StatusCodes.OK)
     expect(result.body).toEqual(message)
   })
-
-  it("0.4.1 Test if the above user was criated", async () => {
-    //falta info sobre o endpoint
-  })
-
-  /* missing output
-    it("0.4.2 Test duplicate email in registration", async () => {
-        const payload = {
-            "role": "intern",
-            "name": "Mário Menezes",
-            "email": "mmenezes@student.dei.uc",
-            "password": "password1234",
-            "address": "Rua do Brigadeiro Cardoso, n 370",
-            "birthDate": "1984-4-13T00:00:00.000Z",
-            "phoneNumber": 919193948
-        }                              
-          
-        // in json format
-        const message = {
-            message: ""
-        }
-
-        const result = await request(app)
-          .post("/api/auth/register")
-          .send(payload)
-          .set("Content-Type", "application/json")
-          .set("Accept", "application/json")
-        expect(result.status).toEqual(StatusCodes.OK)
-        expect(result.body).toEqual(message)
-    })*/
-
-  it("0.4.3 test - Test if the above above test didn't alter user info", async () => {
-    const payload = {
-      email: "mmenezes@student.dei.uc.pt",
-      password: "different_password",
-    }
-
-    const message = {
-      message: "Login. Invalid credentials",
-    }
-
-    const result = await request(app)
-      .post("/api/auth/login")
-      .send(payload)
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-    expect(result.status).toEqual(StatusCodes.UNAUTHORIZED)
-    expect(result.body).toEqual(message)
-  })
 })
-
-/* missing output
-    it("0.11 test - Wrong mail", async () => {
-        const payload = {
-            "role": "therapist",
-            "name": "Ana Sofia",
-            "email": "anasofia@gmail.com",
-            "password": "password1234",
-            "address": "Rua flores",
-            "birthDate": "1990-11-21T00:00:00.000Z",
-            "phoneNumber": 929231943
-        }                              
-          
-        const message = {
-            message: ""
-        }
-
-        const result = await request(app)
-          .post("/api/auth/register")
-          .send(payload)
-          .set("Content-Type", "application/json")
-          .set("Accept", "application/json")
-        expect(result.status).toEqual(StatusCodes.OK)
-        expect(result.body).toEqual(message)
-    })*/
--- api/test/auth/reset-password.test.ts
+++ api/test/auth/reset-password.test.ts
@@ -8,11 +8,11 @@ dotenv.config()
 
 import app from "../../src/main"
 
-describe("(3.0 Test password reset", () => {
-  it("3.0.0 existing user sends request to change password", async () => {
+describe("0.6 Test password reset", () => {
+  it("0.6.0 existing user sends request to change password", async () => {
+    const token = "" // need to get token here from email
     const payload = {
-      email: "johndoe@student.dei.uc.pt",
-      callback: "http://frontend.com/password-reset-page",
+      token : token
     }
 
     const result = await request(app)
@@ -21,40 +21,13 @@ describe("(3.0 Test password reset", () => {
       .set("Content-Type", "application/json")
       .set("Accept", "application/json")
     expect(result.status).toEqual(StatusCodes.OK)
-    // falta o código de sucesso na documentação então não sei o que esperar
   })
 
-  //need to save this token and use it on the next one
-
-  /* missing token - i don't know how to generate
-    it("3.0.1 test - token is valid", async () => {
-
-        const payload = {
-            "token": "token_gerado"
-        }
-        
-        // in json format
-        const message = {
-            message: "Password Reset Successful!"
-        }
-          
-        const result = await request(app)
-          .post("/api/auth/register")
-          .send(payload)
-          .set("Content-Type", "application/json")
-          .set("Accept", "application/json")
-        expect(result.status).toEqual(StatusCodes.OK)
-        expect(result.body).toEqual(message)
-    })*/
-})
-
-describe("(3.1 Test password reset with an invalid token", () => {
-  it("3.1.0 Password reset with invalid token", async () => {
+  it("0.6.1 Password reset with invalid token", async () => {
     const payload = {
       token: "invalid token",
     }
 
-    // in json format
     const message = {
       message: "Password reset token invalid or expired!",
     }
@@ -68,3 +41,4 @@ describe("(3.1 Test password reset with an invalid token", () => {
     expect(result.body).toEqual(message)
   })
 })
+
