048ead4bb7b3ff0758923b8d541c26908359e7b3
DreSilva <DreSilva@users.noreply.github.com>
2023-01-26 12:29:27 +0000

    Apply formatting changes

--- api/test/auth/account-verification.test.ts
+++ api/test/auth/account-verification.test.ts
@@ -10,7 +10,7 @@ import app from "../../src/main"
 import exp from "constants"
 
 describe("0.4 test account verification token", () => {
-   it("0.4.0 account verified", async () => {
+  it("0.4.0 account verified", async () => {
     const payload = {
       email: "johndoe@student.dei.uc.pt",
     } // no call back means that a mail is not sent and the token is sent in the reponse body
@@ -22,13 +22,12 @@ describe("0.4 test account verification token", () => {
       .set("Accept", "application/json")
     expect(result.status).toEqual(StatusCodes.OK)
     expect(result.body).toHaveProperty("token")
-   })
+  })
 
   it("0.4.1 the user is not registered in the application", async () => {
     const payload = {
       email: "email@student.dei.uc.pt",
     } // no call back means that a mail is not sent and the token is sent in the reponse body
-
 
     const result = await request(app)
       .post("/api/auth/account-verification")
