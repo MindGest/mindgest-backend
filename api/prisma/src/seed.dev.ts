557ed128ab374676b4af4fd107645d730ee5a797
JotaCruz20 <JotaCruz20@users.noreply.github.com>
2023-01-27 23:34:07 +0000

    Apply formatting changes

--- api/prisma/src/seed.dev.ts
+++ api/prisma/src/seed.dev.ts
@@ -39,12 +39,12 @@ async function seed() {
   })
 
   let therapist = await prisma.therapist.create({
-    data:{
+    data: {
       extern: false,
       license: "blabla",
       health_system: "blabla",
-      person_id: person1.id
-    }
+      person_id: person1.id,
+    },
   })
 
   let person2 = await prisma.person.create({
@@ -63,36 +63,35 @@ async function seed() {
   })
 
   let patientType = await prisma.patienttype.create({
-    data:{
-      type:"burro"
-    }
+    data: {
+      type: "burro",
+    },
   })
 
-
   let utente = await prisma.patient.create({
-    data:{
-      health_number:12231,
+    data: {
+      health_number: 12231,
       request: "burro",
       remarks: "burro e parvo",
       patienttype_id: patientType.id,
-      person_id: person2.id
-    }
+      person_id: person2.id,
+    },
   })
 
   await prisma.profession.create({
-    data:{
-      name:"proxeneta",
-      patient_person_id: utente.person_id
-    }
+    data: {
+      name: "proxeneta",
+      patient_person_id: utente.person_id,
+    },
   })
 
   await prisma.school.create({
-    data:{
-      name:"escola dos burros",
-      course:"burrice",
-      grade:18,
-      patient_person_id:person2.id
-    }
+    data: {
+      name: "escola dos burros",
+      course: "burrice",
+      grade: 18,
+      patient_person_id: person2.id,
+    },
   })
 
   let person3 = await prisma.person.create({
@@ -111,9 +110,9 @@ async function seed() {
   })
 
   let accountant = await prisma.accountant.create({
-    data:{
-      person_id: person3.id
-    }
+    data: {
+      person_id: person3.id,
+    },
   })
 
   let person4 = await prisma.person.create({
@@ -132,9 +131,9 @@ async function seed() {
   })
 
   let guard = await prisma.guard.create({
-    data:{
-      person_id: person4.id
-    }
+    data: {
+      person_id: person4.id,
+    },
   })
 
   let person5 = await prisma.person.create({
@@ -153,9 +152,9 @@ async function seed() {
   })
 
   let intern = await prisma.intern.create({
-    data:{
-      person_id: person5.id
-    }
+    data: {
+      person_id: person5.id,
+    },
   })
 
   let person6 = await prisma.person.create({
@@ -174,129 +173,128 @@ async function seed() {
   })
 
   let admin = await prisma.admin.create({
-    data:{
-      person_id: person6.id
-    }
+    data: {
+      person_id: person6.id,
+    },
   })
 
   let speciality = await prisma.speciality.create({
-    data:{
+    data: {
       speciality: "burrice",
       code: "burr123",
-      description: "bued burros"
-    }
+      description: "bued burros",
+    },
   })
 
   let process = await prisma.process.create({
-    data:{
-      ref:"tard123",
-      active:true,
-      remarks:"é bue burro",
+    data: {
+      ref: "tard123",
+      active: true,
+      remarks: "é bue burro",
       speciality_speciality: speciality.speciality,
-    }
+    },
   })
 
   await prisma.therapist_speciality.create({
-    data:{
-      speciality_speciality:speciality.speciality,
-      therapist_person_id: person1.id
-    }
+    data: {
+      speciality_speciality: speciality.speciality,
+      therapist_person_id: person1.id,
+    },
   })
 
   await prisma.therapist_process.create({
-    data:{
-      therapist_person_id:person1.id,
-      process_id:process.id
-    }
+    data: {
+      therapist_person_id: person1.id,
+      process_id: process.id,
+    },
   })
 
   await prisma.patient_process.create({
-    data:{
-      process_id:process.id,
-      patient_person_id: person2.id
-    }
+    data: {
+      process_id: process.id,
+      patient_person_id: person2.id,
+    },
   })
 
   let room1 = await prisma.room.create({
-    data:{
-      name:"sala para burros"
-    }
+    data: {
+      name: "sala para burros",
+    },
   })
 
   let pricetable1 = await prisma.pricetable.create({
-    data:{
-      id:"burritos",
-      type:"burritos",
-      price:23.2
-    }
+    data: {
+      id: "burritos",
+      type: "burritos",
+      price: 23.2,
+    },
   })
 
   let appointment1 = await prisma.appointment.create({
-    data:{
+    data: {
       online: false,
-      room_id:room1.id,
-      pricetable_id:pricetable1.id,
-      slot_start_date:new Date("2023-01-31T22:50:28.538Z"),
-      slot_end_date:new Date("2023-01-31T03:50:28.538Z"),
+      room_id: room1.id,
+      pricetable_id: pricetable1.id,
+      slot_start_date: new Date("2023-01-31T22:50:28.538Z"),
+      slot_end_date: new Date("2023-01-31T03:50:28.538Z"),
       active: false,
       archived_date: new Date("2023-01-31T22:50:28.538Z"),
-    }
+    },
   })
 
   let appointment2 = await prisma.appointment.create({
-    data:{
+    data: {
       online: false,
-      room_id:room1.id,
-      pricetable_id:pricetable1.id,
-      slot_start_date:new Date("2023-01-22T22:50:28.538Z"),
-      slot_end_date:new Date("2023-01-22T03:50:28.538Z"),
+      room_id: room1.id,
+      pricetable_id: pricetable1.id,
+      slot_start_date: new Date("2023-01-22T22:50:28.538Z"),
+      slot_end_date: new Date("2023-01-22T03:50:28.538Z"),
       active: true,
       archived_date: new Date("2023-01-22T22:50:28.538Z"),
-    }
+    },
   })
 
   let appointment3 = await prisma.appointment.create({
-    data:{
+    data: {
       online: false,
-      room_id:room1.id,
-      pricetable_id:pricetable1.id,
-      slot_start_date:new Date("2023-01-30T22:50:28.538Z"),
-      slot_end_date:new Date("2023-01-30T03:50:28.538Z"),
+      room_id: room1.id,
+      pricetable_id: pricetable1.id,
+      slot_start_date: new Date("2023-01-30T22:50:28.538Z"),
+      slot_end_date: new Date("2023-01-30T03:50:28.538Z"),
       active: false,
       archived_date: new Date("2023-01-30T22:50:28.538Z"),
-    }
+    },
   })
 
   await prisma.appointment_process.create({
-    data:{
-      process_id:process.id,
-      appointment_slot_id:appointment1.slot_id
-    }
+    data: {
+      process_id: process.id,
+      appointment_slot_id: appointment1.slot_id,
+    },
   })
 
   await prisma.appointment_process.create({
-    data:{
-      process_id:process.id,
-      appointment_slot_id:appointment2.slot_id
-    }
+    data: {
+      process_id: process.id,
+      appointment_slot_id: appointment2.slot_id,
+    },
   })
 
   await prisma.appointment_process.create({
-    data:{
-      process_id:process.id,
-      appointment_slot_id:appointment3.slot_id
-    }
+    data: {
+      process_id: process.id,
+      appointment_slot_id: appointment3.slot_id,
+    },
   })
 
   let receipt1 = await prisma.receipt.create({
-    data:{
-      ref:"istoechato",
+    data: {
+      ref: "istoechato",
       datetime: new Date("2023-01-22T22:50:28.538Z"),
       payed: true,
-      appointment_slot_id: appointment2.slot_id
-    }
+      appointment_slot_id: appointment2.slot_id,
+    },
   })
 }
-
 
 export default { seed }
